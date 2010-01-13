35b9889eba1e50391b9432b9d97c8d85a0dfbca5
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-01-13 10:14:09 +0000

    * Bugfixes and cleanup

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -4,10 +4,10 @@
 
 CREATE TABLE `ext_timetracking_track` (
   `id` int(11) NOT NULL auto_increment,
-  `date_update` int(10) unsigned NOT NULL,
   `date_create` int(10) unsigned NOT NULL default '0',
-  `date_track` int(10) unsigned NOT NULL default '0',
+  `date_update` int(10) unsigned NOT NULL,
   `id_user_create` int(10) unsigned NOT NULL default '0',
+  `date_track` int(10) unsigned NOT NULL default '0',
   `id_task` int(10) unsigned NOT NULL default '0',
   `workload_tracked` int(10) unsigned NOT NULL default '0',
   `workload_chargeable` int(10) unsigned NOT NULL default '0',
