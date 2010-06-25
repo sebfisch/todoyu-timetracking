928e5ccebebf00cd292cf5a0644e5829e64c3c69
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-06-25 09:38:13 +0000

    * adjusted all SQL files to comply with coding convention

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -3,17 +3,17 @@
 --
 
 CREATE TABLE `ext_timetracking_track` (
-  `id` int(10) NOT NULL auto_increment,
-  `date_create` int(10) unsigned NOT NULL default '0',
-  `date_update` int(10) unsigned NOT NULL,
-  `id_person_create` int(10) unsigned NOT NULL default '0',
-  `date_track` int(10) unsigned NOT NULL default '0',
-  `id_task` int(10) unsigned NOT NULL default '0',
-  `workload_tracked` int(10) unsigned NOT NULL default '0',
-  `workload_chargeable` int(10) unsigned NOT NULL default '0',
-  `comment` text NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `task` (`id_task`)
+	`id` int(10) NOT NULL auto_increment,
+	`date_create` int(10) unsigned NOT NULL default '0',
+	`date_update` int(10) unsigned NOT NULL,
+	`id_person_create` int(10) unsigned NOT NULL default '0',
+	`date_track` int(10) unsigned NOT NULL default '0',
+	`id_task` int(10) unsigned NOT NULL default '0',
+	`workload_tracked` int(10) unsigned NOT NULL default '0',
+	`workload_chargeable` int(10) unsigned NOT NULL default '0',
+	`comment` text NOT NULL,
+	PRIMARY KEY  (`id`),
+	KEY `task` (`id_task`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -23,10 +23,10 @@ CREATE TABLE `ext_timetracking_track` (
 --
 
 CREATE TABLE `ext_timetracking_tracking` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_task` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_task` int(10) unsigned NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
