fa1fe4a3864549b9d828e8525641a51c293b0868
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-10-18 08:50:55 +0000

    * Translation: Polish

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['timetracking']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state' 		=> 'stable',
-	'version' 		=> '1.1.1',
+	'version' 		=> '1.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'contact'	=> '1.1.0',
