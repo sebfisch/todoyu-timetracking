e3e45019bc938768ec386ef46a4cabc92063a4ca
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-12-02 13:18:28 +0000

    * Bugfix: Subtasks were marked as running too

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['timetracking']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state' 		=> 'stable',
-	'version' 		=> '1.1.2',
+	'version' 		=> '1.1.3',
 	'constraints' => array(
 		'depends' => array(
 			'contact'	=> '1.1.0',
