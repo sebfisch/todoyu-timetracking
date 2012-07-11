92ee04ff8f07cc8858dde913e5006397204d9bc7
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-07-11 15:29:31 +0000

    * Version update

--- config/extinfo.php
+++ config/extinfo.php
@@ -33,10 +33,10 @@ Todoyu::$CONFIG['EXT']['timetracking']['info'] = array(
 	'state'			=> 'stable',
 	'version'		=> '1.3.0',
 	'constraints' => array(
-		'core'		=> '2.2.0',
+		'core'		=> '2.3.0',
 		'depends'	=> array(
-			'contact'	=> '1.2.0',
-			'project' 	=> '1.3.0'
+			'contact'	=> '1.3.0',
+			'project' 	=> '1.4.0'
 		)
 	),
 	'urlDocumentation'	=> 'http://doc.todoyu.com/?timetracking'
